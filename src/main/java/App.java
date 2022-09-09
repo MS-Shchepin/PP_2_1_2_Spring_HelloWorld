8f0c4e4e0d1b12a5ea8a6b0496588e8cb4e72182
MS-Shchepin <promayhem@mail.ru>
2022-09-09 19:02:51 +0300

    Solved

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,11 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld hw = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == hw);
+        System.out.println(cat1 == cat2);
         System.out.println(bean.getMessage());
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,37 @@
+import java.util.Objects;
+
+public class Cat {
+    private String name;
+
+    public Cat() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return Objects.equals(name, cat.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Cat{");
+        sb.append("name='").append(name).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
